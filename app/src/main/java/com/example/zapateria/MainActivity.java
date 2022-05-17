d1e2b4d5e35b742767893197c7b92b7ddc7f8103
BryanUGB <bryanpineda2001@gmail.com>
2022-05-16 22:28:47 -0600

    Se establecio conexion con fire base (POR: SMIS941220)

--- app/src/main/java/com/example/zapateria/MainActivity.java
+++ app/src/main/java/com/example/zapateria/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zapateria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                   sleep(2000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
