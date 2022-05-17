b05f6a37c85884756eb0ce7441c21a204c001d1e
BryanUGB <bryanpineda2001@gmail.com>
2022-05-17 12:29:48 -0600

    "Se creo los botones de usuario y administrador con sus activity correcpondientes"

--- app/src/main/java/com/example/zapateria/HomeActivity.java
+++ app/src/main/java/com/example/zapateria/HomeActivity.java
@@ -2,13 +2,42 @@ package com.example.zapateria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button botonusuario, botonadministrador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        botonusuario=(Button)findViewById(R.id.botonusuario);
+        botonadministrador=(Button)findViewById(R.id.botonadministrador);
+
+        botonusuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        botonadministrador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LoginadminActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
 }
